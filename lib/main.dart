e0b43f299873f27d62e57c3802e962364c6e24b3
msaidguven <msadguven@gmail.com>
2026-02-08 23:29:23 +0300

    login_register

--- lib/main.dart
+++ lib/main.dart
@@ -82,7 +82,7 @@ class _EgitimUygulamasiState extends State<EgitimUygulamasi> {
       routes: {
         '/reset-password': (context) => const ResetPasswordScreen(),
         '/login': (context) => const LoginScreen(),
-        '/register': (context) => const SignupScreen(),
+        '/register': (context) => SignupScreen(),
       },
     );
   }
